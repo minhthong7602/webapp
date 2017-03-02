8d19a81a2acff60899dec5a217040b4cfaf3cbdc
minhthong7602 <minhthongbkhn1994@gmail.com>
2017-03-02 14:50:49 +0700

    Add About,Book Controller

---
+++ src/WepApp/Controllers/AboutController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WepApp.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly IStringLocalizer<AboutController> _localizer;
+
+        public AboutController(IStringLocalizer<AboutController> localizer)
+        {
+            this._localizer = localizer;
+        }
+        // GET: /<controller>/
+        [HttpGet]
+        public string Get()
+        {
+            return _localizer["About Title"];
+        }
+
+
+    }
+}
---
+++ src/WepApp/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WepApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IHtmlLocalizer<BookController> _localizer;
+
+        public BookController(IHtmlLocalizer<BookController> localizer)
+        {
+            this._localizer = localizer;
+        }
+        // GET: /<controller>/
+        public IActionResult Hello(string name)
+        {
+            ViewData["Message"] = _localizer["<b>Hello</b><i> {0} </i>", name];
+            return View();
+        }
+    }
+}
